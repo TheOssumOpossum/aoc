9beae97a34f07b69f6897765e263fa518bd4a514
TheOssumOpossum <v.bommier@gmail.com>
2020-12-03 00:54:39 -0500

    add to template

--- 2020/template.py
+++ 2020/template.py
@@ -1,6 +1,35 @@
 # https://adventofcode.com/2020/day/1
+import re
 
-day = "01"
+day = "0"
 
-input = open("../input/day" + day + ".txt", "r")
-# input = open("../tst/day" + day + "_test.txt", "r")
+# txt = open("../input/day" + day + ".txt", "r")
+txt = open("../tst/day" + day + "_test.txt", "r")
+
+dic = {}
+i = 0
+max_len = 0
+
+for l in txt:
+    #Get max length of line
+    # if len(l) > max_len:
+    #     max_len = len(l)
+
+    #Build 2D Map
+    # j = 0
+    # for c in l:
+    #     if c == '\n':
+    #         continue
+    #     dic[(i,j)] = c
+    #     j += 1
+
+    #Split line into terms by the delimiters
+    # delimiters = [' ', ',' , '\:' ]
+    # rex_split_string = ''.join([c + '|' for c in delimiters])[:-1]
+    # terms = re.split(rex_split_string, l[:-1])
+    # dic[i] = terms
+
+    #Increment row number
+    i += 1
+
+lines = i
